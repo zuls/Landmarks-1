7964f094d51f2fb72d325d60dfb17c24157b5230
zuls <zularbine@gmail.com>
2025-05-22 12:26:44 -0400

    Initial Commit

---
+++ Landmarks-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmarks-1
+//
+//  Created by Zularbine Kamal on 5/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks-1/Landmarks_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_1App.swift
+//  Landmarks-1
+//
+//  Created by Zularbine Kamal on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
