61b54c35d5418d7212a956c0a9b713be1a33a612
zuls <zularbine@gmail.com>
2025-05-26 11:32:02 -0400

    completed listing and location details view

---
+++ Landmarks-1/LandmarkList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkList.swift
+//  Landmarks-1
+//
+//  Created by Zularbine Kamal on 5/22/25.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
