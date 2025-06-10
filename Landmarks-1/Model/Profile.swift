b53ee9cee55a0a6147533c6dd933849e15e5ba23
zuls <zularbine@gmail.com>
2025-06-10 11:42:06 -0400

    completed hikeview

---
+++ Landmarks-1/Model/Profile.swift
@@ -0,0 +1,8 @@
+//
+//  Profile.swift
+//  Landmarks-1
+//
+//  Created by Zularbine Kamal on 6/9/25.
+//
+
+import Foundation
---
+++ Landmarks-1/Views/Categories/CategoryItem.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryItem.swift
+//  Landmarks-1
+//
+//  Created by Zularbine Kamal on 6/4/25.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryItem()
+}
