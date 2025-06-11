7015be12ec8029edac2822404b29d6bf2b8b7de9
zuls <zularbine@gmail.com>
2025-06-11 18:59:27 -0400

    completed befoe UIkit integration

---
+++ Landmarks-1/Views/Hikes/HikeBadge.swift
@@ -0,0 +1,18 @@
+//
+//  HikeBadge.swift
+//  Landmarks-1
+//
+//  Created by Zularbine Kamal on 6/10/25.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HikeBadge()
+}
